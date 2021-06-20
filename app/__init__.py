d1dc8879483a98cee885371f1296cd5eca33782b
Your Name <you@example.com>
2021-06-20 19:17:10 +0300

    created migrations and initialized db

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import config
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -7,5 +9,7 @@ def create_app(config_name):
 
     from . main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    db.init_app(app)
 
     return app
