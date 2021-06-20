5fcaf3ff8161290ed7e033c6c84c7ca6f0a17d65
Your Name <you@example.com>
2021-06-20 19:28:36 +0300

    configures models

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
 from . import main,db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin, login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
 posts = [
     {
         'author': 'Manada Butate',
@@ -19,7 +24,7 @@ posts = [
     }
 ]
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -41,7 +46,7 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-class Post(db.Model):
+class Post(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
