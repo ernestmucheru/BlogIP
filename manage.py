89172529ad7f78fa488d8d41fc297912b2351e85
Your Name <you@example.com>
2021-06-20 19:12:28 +0300

    initialized flask migrate

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 import os
-from app import create_app
+from app import create_app,db
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand, migrate
+
 
 app = create_app(os.getenv('FLASK_CONFIG')or 'default')
 manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 
 if __name__ == '__main__':
     manager.run()
