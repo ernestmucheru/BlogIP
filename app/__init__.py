291abfd4b57cfd4f07208e104a3bc586c1d158dd
Your Name <you@example.com>
2021-06-20 19:25:41 +0300

    initialized login manager in create app function

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+from flask_login import LoginManager, login_manager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -14,5 +19,6 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
     db.init_app(app)
+    login_manager.init_app(app)
 
     return app
