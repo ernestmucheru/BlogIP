3e5962595ed1e4f2b657824cac60ad53fc91fc41
Your Name <you@example.com>
2021-06-20 14:39:28 +0300

    initial file structure

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from . import main
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+    return render_template('user.html', username=username)
