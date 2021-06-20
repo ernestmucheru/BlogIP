e41c5809083ffc2ba34e58cb1f46e17e12c6bdb3
Your Name <you@example.com>
2021-06-20 19:44:20 +0300

    installed email validator

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from . import main,db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin, login_manager
+from . import login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-    
 posts = [
     {
         'author': 'Manada Butate',
