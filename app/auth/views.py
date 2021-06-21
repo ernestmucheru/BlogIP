6f2461805f7672aa09526b56c8e0f9facaa18525
Your Name <you@example.com>
2021-06-21 16:24:20 +0300

    made changes to update post function

--- app/auth/views.py
+++ app/auth/views.py
@@ -120,6 +120,8 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash('Your post has been updated!', 'success')
-    form.title.data = post.title
-    form.content.data = post .content
+        return redirect(url_for('post',post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post .content
     return render_template('create_post.html', title='Update Post', legend ='Update Post', form=form)
