265535bf4028388daebf54ea0d23516c1da3d42c
Your Name <you@example.com>
2021-06-21 21:09:17 +0300

    made changes to update post feature

--- app/main/views.py
+++ app/main/views.py
@@ -1,31 +1,31 @@
-from flask import render_template,url_for,flash,redirect,request
+from flask import render_template
 from . import main
 from flask_login import login_required
 
 
-posts = [
-    {
-        'author': 'Manada Butate',
-        'title': 'New Blog',
-        'content': 'Mambo Vipi',
-        'date_posted': 'April 12, 2021'
+# posts = [
+#     {
+#         'author': 'Manada Butate',
+#         'title': 'New Blog',
+#         'content': 'Mambo Vipi',
+#         'date_posted': 'April 12, 2021'
         
-    },
-    {
-        'author': 'Tika Ingoha',
-        'title': 'Trending Blod',
-        'content': 'Haba na Haba',
-        'date_posted': 'April 21, 2021'
+#     },
+#     {
+#         'author': 'Tika Ingoha',
+#         'title': 'Trending Blod',
+#         'content': 'Haba na Haba',
+#         'date_posted': 'April 21, 2021'
         
-    }
-]
+#     }
+# ]
 
 
 
 @main.route('/')
 @login_required
 def home():
-    return render_template('home.html', posts = posts)
+    return render_template('home.html')
 
 @main.route('/about')
 def about():
